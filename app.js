551026480ffadd8589a509a73e350062a03e3b35
JayantVerman <sixcasesixfile@gmail.com>
2023-07-31 11:47:31 +0530

    Level6 -OAuth2.0 & How to implement Sign In wiwth Google

--- app.js
+++ app.js
@@ -1,4 +1,4 @@
-// require('dotenv').config();
+require('dotenv').config();
 const express = require('express');
 const bodyParser = require('body-parser');
 const ejs = require('ejs');
@@ -6,6 +6,9 @@ const mongoose=require('mongoose');
 const session= require('express-session');
 const passport = require('passport');
 const passportLocalMongoose=require('passport-local-mongoose');
+const GoogleStrategy = require('passport-google-oauth20').Strategy;
+const findOrCreate=require('mongoose-findorcreate');
+
 // const bcrypt = require('bcrypt');
 // const saltRounds=10;
 
@@ -35,7 +38,8 @@ mongoose.connect('mongodb://127.0.0.1:27017/userDB');
 // creating schema
 const userSchema=new mongoose.Schema({
 email:String,
-password:String
+password:String,
+googleId:String
 });
 
 // // database encryption using key mongoose encryption
@@ -45,16 +49,39 @@ password:String
 //      encryptedFields:["password"]});
 
 userSchema.plugin(passportLocalMongoose);
-
+userSchema.plugin(findOrCreate);
 //db model
 const User=mongoose.model('User',userSchema);
   
 passport.use(User.createStrategy());
 
-passport.serializeUser(User.serializeUser());
-passport.deserializeUser(User.deserializeUser());
+passport.serializeUser(function(user,done){
+    done(null,user.id);
+});
+passport.deserializeUser((id, done) => {
+    User.findById(id)
+        .then(user => {
+            done(null, user);
+        })
+        .catch(error => {
+            done(error, null);
+        });
+});
 
 
+passport.use(new GoogleStrategy({
+    clientID: process.env.CLIENT_ID,
+    clientSecret: process.env.CLIENT_SECRET,
+    callbackURL: "http://localhost:3000/auth/google/secrets",
+    // userProfileURL:"https://www.googleapis.com/oauth2/v3/userinfo"
+  },
+  function(accessToken, refreshToken, profile, cb) {
+    
+    User.findOrCreate({ googleId: profile.id }, function (err, user) {
+      return cb(err, user);
+    });
+  }
+));
 
 //routes logic starts here
 app.get('/',(req, res) => {
@@ -75,6 +102,17 @@ res.render('login');
 app.get('/register',(req, res) => {
 res.render('register');
 });
+
+app.get('/auth/google',
+    passport.authenticate('google',{scope:["profile"]})
+);
+
+app.get('/auth/google/secrets',
+    passport.authenticate('google',{failureRedirect:'/login'}),function(req,res){
+       //successfull authentication, redirect to secrets.
+        res.redirect('/secrets');
+    }
+);
 
 app.get('/secrets',(req, res) => {
     if(req.isAuthenticated()){
